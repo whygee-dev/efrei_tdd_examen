dd2522783ef37957b2cc6207b6ce320abaf8c9aa
Yahya JIRARI <whygee.dev@gmail.com>
2024-02-07 17:29:44 +0100

    Add more tests to canMovePiece, covering pieces movement implementation

--- src/ChessGame.spec.ts
+++ src/ChessGame.spec.ts
@@ -25,11 +25,97 @@ describe("ChessGame", () => {
   });
 
   describe("canMovePiece", () => {
-    it("should return true if a piece can move to a new position", () => {
+    it("should return true if a pawn is trying to move to two squares forward", () => {
       // Arrange
       const game = new ChessGame();
-      const initialSquare = "e2"; 
-      const newSquare = "e4"; 
+      const initialSquare = "e2";
+      const newSquare = "e4";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(true);
+    });
+
+    it("should return true if a pawn is trying to move to one square forward", () => {
+      // Arrange
+      const game = new ChessGame();
+      const initialSquare = "f2";
+      const newSquare = "f3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(true);
+    });
+
+    it("should return true if a bishop is trying to move diagonally", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("e2", "e4");
+
+      const initialSquare = "f1";
+      const newSquare = "c4";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(true);
+    });
+
+    it("should return true if a king is trying to move to a L-position one square forward and one square to the right", () => {
+      // Arrange
+      const game = new ChessGame();
+      const initialSquare = "b1";
+      const newSquare = "c3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(true);
+    });
+
+    it("should return true if a rook is trying to move to a square in the same column", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("a2", "a4");
+
+      const initialSquare = "a1";
+      const newSquare = "a3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(true);
+    });
+
+    it("should return true if a queen is trying to move to a square in the same row", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("d2", "d4");
+
+      const initialSquare = "d1";
+      const newSquare = "d3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(true);
+    });
+
+    it("should return true if a queen is trying to move diagonally", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("c2", "c4");
+
+      const initialSquare = "d1";
+      const newSquare = "a4";
 
       // Act
       const canMove = game.canMovePiece(initialSquare, newSquare);
@@ -64,9 +150,150 @@ describe("ChessGame", () => {
       expect(canMove).toBe(false);
     });
 
-    // Add more test cases for different scenarios
-});
+    it("should return false if a pawn tries to move two squares forward from a non-starting position", () => {
+      // Arrange
+      const game = new ChessGame();
+      const initialSquare = "e4";
+      const newSquare = "e6";
 
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if a pawn tries to move more than two squares forward", () => {
+      // Arrange
+      const game = new ChessGame();
+      const initialSquare = "e2";
+      const newSquare = "e5";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if a pawn tries to move more than one square sideways", () => {
+      // Arrange
+      const game = new ChessGame();
+      const initialSquare = "e2";
+      const newSquare = "g2";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if the king tries to move more than one square forward", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("e2", "e4");
+
+      const initialSquare = "e1";
+      const newSquare = "e3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if the king tries to move more than one square sideways", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("e2", "e4");
+
+      const initialSquare = "e1";
+      const newSquare = "g1";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if the king tries to move more than one square diagonally", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("f2", "f4");
+
+      const initialSquare = "e1";
+      const newSquare = "g4";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if a bishop tries to move to a non-diagonal position", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("f2", "f4");
+
+      const initialSquare = "f1";
+      const newSquare = "f3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if a rook tries to move diagonally", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("g2", "g4");
+
+      const initialSquare = "h1";
+      const newSquare = "g2";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if a knight tries to move to a non-L position", () => {
+      // Arrange
+      const game = new ChessGame();
+      const initialSquare = "g1";
+      const newSquare = "g3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    it("should return false if a queen tries to move like a knight", () => {
+      // Arrange
+      const game = new ChessGame();
+      game.movePiece("d2", "d4");
+
+      const initialSquare = "d1";
+      const newSquare = "e3";
+
+      // Act
+      const canMove = game.canMovePiece(initialSquare, newSquare);
+
+      // Assert
+      expect(canMove).toBe(false);
+    });
+
+    // TODO: Add more tests for pieces
+  });
 
   describe("movePiece", () => {
     it("should move a piece from a2 to a3", () => {
@@ -90,22 +317,22 @@ describe("ChessGame", () => {
       expect(game.board).toEqual(expectedBoard);
     });
 
-    it("should move a piece from a7 to a3", () => {
+    it("should move a piece from a7 to a5", () => {
       // Arrange
       const game = new ChessGame();
       const expectedBoard = [
         ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
         [" ", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
         [" ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " "],
         ["♟", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
         ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
         ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
       ];
 
       // Act
-      game.movePiece("a7", "a3");
+      game.movePiece("a7", "a5");
 
       // Assert
       expect(game.board).toEqual(expectedBoard);
@@ -117,18 +344,18 @@ describe("ChessGame", () => {
       // Arrange
       const game = new ChessGame();
       game.movePiece("g2", "g3");
-      game.movePiece("g1", "g4");
+      game.movePiece("g1", "h3");
       game.movePiece("f2", "f3");
-      game.movePiece("f1", "f2");
+      game.movePiece("f1", "g2");
 
       const expectedBoard = [
         ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
         ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
         [" ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", "♘", " "],
-        [" ", " ", " ", " ", " ", "♙", "♙", " "],
-        ["♙", "♙", "♙", "♙", "♙", "♗", " ", "♙"],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", "♙", "♙", "♘"],
+        ["♙", "♙", "♙", "♙", "♙", " ", "♗", "♙"],
         ["♖", "♘", "♗", "♕", " ", "♖", "♔", " "],
       ];
 
@@ -142,10 +369,10 @@ describe("ChessGame", () => {
     it("should correctly castle queen side if conditions are met", () => {
       // Arrange
       const game = new ChessGame();
+      game.movePiece("a2", "a3");
       game.movePiece("b2", "b3");
-      game.movePiece("b1", "b4");
-      game.movePiece("c2", "c3");
-      game.movePiece("c1", "c2");
+      game.movePiece("b1", "c3");
+      game.movePiece("c1", "b2");
       game.movePiece("d2", "d3");
       game.movePiece("d1", "d2");
 
@@ -154,9 +381,9 @@ describe("ChessGame", () => {
         ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
         [" ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", "♘", " ", " ", " ", " ", " ", " "],
-        [" ", "♙", "♙", "♙", " ", " ", " ", " "],
-        ["♙", " ", "♗", "♕", "♙", "♙", "♙", "♙"],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        ["♙", "♙", "♘", "♙", " ", " ", " ", " "],
+        [" ", "♗", "♙", "♕", "♙", "♙", "♙", "♙"],
         [" ", " ", "♔", "♖", " ", "♗", "♘", "♖"],
       ];
 
@@ -166,6 +393,8 @@ describe("ChessGame", () => {
       // Assert
       expect(game.board).toEqual(expectedBoard);
     });
+
+    // TODO: implement same tests as two above for black
 
     it("should throw an error while castling(white, king) if king has already moved", () => {
       // Arrange
@@ -254,34 +483,34 @@ describe("ChessGame", () => {
         [" ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " "],
       ];
-  
+
       game["_board"] = initialBoard;
-  
+
       const squareToKill = "b1";
-  
+
       // Act
       game.killPiece(squareToKill);
-  
+
       // Assert
       expect(game["_board"][1][1]).toBe(" ");
     });
-    
+
     it("should throw an error if trying to kill a piece at an invalid position", () => {
       // Arrange
       const game = new ChessGame();
       const invalidSquare = "k10";
-    
+
       // Act & Assert
       expect(() => {
         game.killPiece(invalidSquare);
       }).toThrowError("Invalid position");
     });
-    
+
     it("should throw an error if there is no piece at the specified position", () => {
       // Arrange
       const game = new ChessGame();
       const emptySquare = "c3"; // Empty position
-    
+
       // Act & Assert
       expect(() => {
         game.killPiece(emptySquare);
--- src/ChessGame.ts
+++ src/ChessGame.ts
@@ -34,7 +34,11 @@ export class ChessGame {
     return board.join("\n");
   }
 
-  movePiece(from: string, to: string) {
+  movePiece(from: string, to: string, castling = false) {
+    if (!this.canMovePiece(from, to) && !castling) {
+      throw new Error("Can't move piece to the specified position: " + from + " ->  " + to);
+    }
+
     const fromCoords = this.getCoords(from);
     const toCoords = this.getCoords(to);
 
@@ -122,11 +126,11 @@ export class ChessGame {
 
     if (color === "white") {
       if (side === "king") {
-        this.movePiece("e1", "g1");
-        this.movePiece("h1", "f1");
+        this.movePiece("e1", "g1", true);
+        this.movePiece("h1", "f1", true);
       } else {
-        this.movePiece("e1", "c1");
-        this.movePiece("a1", "d1");
+        this.movePiece("e1", "c1", true);
+        this.movePiece("a1", "d1", true);
       }
 
       return;
@@ -134,11 +138,11 @@ export class ChessGame {
 
     if (color === "black") {
       if (side === "king") {
-        this.movePiece("e8", "g8");
-        this.movePiece("h8", "f8");
+        this.movePiece("e8", "g8", true);
+        this.movePiece("h8", "f8", true);
       } else {
-        this.movePiece("e8", "c8");
-        this.movePiece("a8", "d8");
+        this.movePiece("e8", "c8", true);
+        this.movePiece("a8", "d8", true);
       }
 
       return;
@@ -173,6 +177,108 @@ export class ChessGame {
     if (piece === " ") {
       return false;
     }
-    return this.#board[newCoords[0]][newCoords[1]] === " ";
+
+    const rowDiff = Math.abs(newCoords[0] - initialCoords[0]);
+    const colDiff = Math.abs(newCoords[1] - initialCoords[1]);
+
+    switch (piece) {
+      case "♟":
+        if (initialCoords[0] === 1) {
+          if (newCoords[0] === initialCoords[0] + 1 || newCoords[0] === initialCoords[0] + 2) {
+            if (newCoords[1] === initialCoords[1] && this.#board[newCoords[0]][newCoords[1]] === " ") {
+              return true;
+            }
+          }
+        } else {
+          if (newCoords[0] === initialCoords[0] + 1 && newCoords[1] === initialCoords[1] && this.#board[newCoords[0]][newCoords[1]] === " ") {
+            return true;
+          }
+        }
+        break;
+      case "♙":
+        if (initialCoords[0] === 6) {
+          if (newCoords[0] === initialCoords[0] - 1 || newCoords[0] === initialCoords[0] - 2) {
+            if (newCoords[1] === initialCoords[1] && this.#board[newCoords[0]][newCoords[1]] === " ") {
+              return true;
+            }
+          }
+        } else {
+          if (newCoords[0] === initialCoords[0] - 1 && newCoords[1] === initialCoords[1] && this.#board[newCoords[0]][newCoords[1]] === " ") {
+            return true;
+          }
+        }
+        break;
+      case "♜":
+      case "♖":
+        if (rowDiff === 0 || colDiff === 0) {
+          const rowStep = rowDiff === 0 ? 0 : newCoords[0] > initialCoords[0] ? 1 : -1;
+          const colStep = colDiff === 0 ? 0 : newCoords[1] > initialCoords[1] ? 1 : -1;
+          let i = initialCoords[0] + rowStep;
+          let j = initialCoords[1] + colStep;
+
+          while (i !== newCoords[0] || j !== newCoords[1]) {
+            if (this.#board[i][j] !== " ") {
+              return false;
+            }
+            i += rowStep;
+            j += colStep;
+          }
+
+          return true;
+        }
+        break;
+      case "♞":
+      case "♘":
+        if ((rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2)) {
+          return true;
+        }
+        break;
+      case "♝":
+      case "♗":
+        if (rowDiff === colDiff) {
+          const rowStep = newCoords[0] > initialCoords[0] ? 1 : -1;
+          const colStep = newCoords[1] > initialCoords[1] ? 1 : -1;
+          let i = initialCoords[0] + rowStep;
+          let j = initialCoords[1] + colStep;
+
+          while (i !== newCoords[0] || j !== newCoords[1]) {
+            if (this.#board[i][j] !== " ") {
+              return false;
+            }
+            i += rowStep;
+            j += colStep;
+          }
+
+          return true;
+        }
+        break;
+      case "♛":
+      case "♕":
+        if ((rowDiff === 0 && colDiff > 0) || (rowDiff > 0 && colDiff === 0) || rowDiff === colDiff) {
+          const rowStep = rowDiff === 0 ? 0 : newCoords[0] > initialCoords[0] ? 1 : -1;
+          const colStep = colDiff === 0 ? 0 : newCoords[1] > initialCoords[1] ? 1 : -1;
+          let i = initialCoords[0] + rowStep;
+          let j = initialCoords[1] + colStep;
+
+          while (i !== newCoords[0] || j !== newCoords[1]) {
+            if (this.#board[i][j] !== " ") {
+              return false;
+            }
+            i += rowStep;
+            j += colStep;
+          }
+
+          return true;
+        }
+        break;
+      case "♚":
+      case "♔":
+        if (rowDiff <= 1 && colDiff <= 1) {
+          return true;
+        }
+        break;
+    }
+
+    return false;
   }
 }
